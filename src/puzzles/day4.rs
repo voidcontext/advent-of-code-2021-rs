daf22865bef8804f1bc0caf31454ead5821ad9c0
Gabor Pihaj <gabor.pihaj@gmail.com>
2021-12-21 06:44:57 +0000

    Day4: embrace mutability to optimize memory consumption

--- src/puzzles/day4.rs
+++ src/puzzles/day4.rs
@@ -6,20 +6,14 @@ mod parser;
 
 use crate::file_utils::read_and_parse;
 
-type BoardState<'a> = Vec<(&'a u32, bool)>;
+type BoardState = Vec<(u32, bool)>;
 
 // TODO: memory consumption seems to be high, this needs to be investigated
 pub fn solve() {
     let input = read_and_parse("data/day4.input", parser::parse).unwrap();
 
-    let initial_board_states: Vec<BoardState> = input
-        .bingo_boards
-        .iter()
-        .map(|board| board.iter().map(|n| (n, false)).collect())
-        .collect();
-
     let (first_winner, first_winner_index) =
-        find_first_winner(&initial_board_states, &input.numbers_drawn, 0);
+        find_first_winner(&input.bingo_boards, &input.numbers_drawn);
 
     let result_a = calc_board_score(
         &first_winner,
@@ -28,7 +22,7 @@ pub fn solve() {
     println!("Day4 A resull: {:?}", result_a);
 
     let (last_winner, last_winner_index) =
-        find_last_winner(&initial_board_states, &input.numbers_drawn, 0);
+        find_last_winner(&input.bingo_boards, &input.numbers_drawn);
 
     let result_b = calc_board_score(
         &last_winner,
@@ -37,65 +31,63 @@ pub fn solve() {
     println!("Day4 B resull: {:?}", result_b);
 }
 
-fn calc_board_score(board_state: &[(&u32, bool)], last_number: usize) -> usize {
-    board_state.iter().fold(
-        0,
-        |acc, (n, marked)| if *marked { acc } else { acc + (**n) as usize },
-    ) * last_number
-}
-
-fn find_first_winner<'a>(
-    board_states: &[Vec<(&'a u32, bool)>],
-    numbers_drawn: &[u32],
-    index: usize,
-) -> (BoardState<'a>, usize) {
-    let new_state = calc_new_states(board_states, numbers_drawn, index);
-
-    if let Some(winner) = new_state.iter().find(|b| is_winner(b)) {
-        (winner.to_vec(), index)
-    } else {
-        find_first_winner(&new_state, numbers_drawn, index + 1)
-    }
-}
-
-fn find_last_winner<'a>(
-    board_states: &[Vec<(&'a u32, bool)>],
-    numbers_drawn: &[u32],
-    index: usize,
-) -> (BoardState<'a>, usize) {
-    let new_states = calc_new_states(board_states, numbers_drawn, index);
-
-    if new_states.len() == 1 && is_winner(&new_states[0]) {
-        (new_states[0].to_vec(), index)
-    } else {
-        find_last_winner(
-            &new_states
-                .into_iter()
-                .filter(|b| !is_winner(b))
-                .collect::<Vec<_>>(),
-            numbers_drawn,
-            index + 1,
-        )
-    }
-}
-
-fn calc_new_states<'a>(
-    board_states: &[Vec<(&'a u32, bool)>],
-    numbers_drawn: &[u32],
-    index: usize,
-) -> Vec<BoardState<'a>> {
-    board_states
+fn initial_state(boards: &[Vec<u32>]) -> Vec<BoardState> {
+    boards
         .iter()
-        .map(|board_state| {
-            board_state
-                .iter()
-                .map(|(n, marked)| (*n, *marked || **n == numbers_drawn[index]))
-                .collect()
-        })
+        .map(|board| board.iter().map(|n| (*n, false)).collect())
         .collect()
 }
 
-fn is_winner(board_state: &[(&u32, bool)]) -> bool {
+fn calc_board_score(board_state: &[(u32, bool)], last_number: usize) -> usize {
+    board_state.iter().fold(
+        0,
+        |acc, (n, marked)| if *marked { acc } else { acc + (*n) as usize },
+    ) * last_number
+}
+
+fn find_first_winner(boards: &[Vec<u32>], numbers_drawn: &[u32]) -> (BoardState, usize) {
+    let mut state = initial_state(boards);
+    let mut index = 0;
+
+    loop {
+        calc_new_states(&mut state, numbers_drawn, index);
+
+        if let Some(w) = state.iter().find(|b| is_winner(b)) {
+            break (w.clone(), index);
+        }
+
+        index += 1;
+    }
+}
+
+fn find_last_winner(boards: &[Vec<u32>], numbers_drawn: &[u32]) -> (BoardState, usize) {
+    let mut state = initial_state(boards);
+    let mut index = 0;
+
+    loop {
+        calc_new_states(&mut state, numbers_drawn, index);
+
+        if state.len() == 1 && is_winner(&state[0]) {
+            break (state[0].clone(), index);
+        }
+
+        state.retain(|b| !is_winner(b));
+
+        index += 1;
+    }
+}
+
+fn calc_new_states(board_states: &mut [Vec<(u32, bool)>], numbers_drawn: &[u32], index: usize) {
+    for board in board_states {
+        for cell in board {
+            if cell.0 == numbers_drawn[index] {
+                cell.1 = true;
+            }
+        }
+    }
+}
+
+fn is_winner(board_state: &[(u32, bool)]) -> bool {
     let (row_marked, column_marked) =
         (0..5).fold((false, false), |(row_marked, column_marked), i| {
             let (current_row, current_column) =
@@ -119,12 +111,12 @@ mod tests {
     #[test]
     fn test_is_winner_should_returns_true_when_full_row_is_marked() {
         #[rustfmt::skip]
-        let state: Vec<(&u32, bool)> = vec![
-            (&21, false), (&23, true),  (&11, false), (&54, false), (&95, false),
-            (&21, true),  (&23, true),  (&11, true),  (&54, true),  (&95, true),
-            (&21, false), (&23, false), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, false), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, true),  (&11, false), (&54, false), (&95, false),
+        let state: Vec<(u32, bool)> = vec![
+            (21, false), (23, true),  (11, false), (54, false), (95, false),
+            (21, true),  (23, true),  (11, true),  (54, true),  (95, true),
+            (21, false), (23, false), (11, false), (54, false), (95, false),
+            (21, false), (23, false), (11, false), (54, false), (95, false),
+            (21, false), (23, true),  (11, false), (54, false), (95, false),
         ];
 
         assert!(is_winner(&state));
@@ -133,12 +125,12 @@ mod tests {
     #[test]
     fn test_is_winner_should_returns_true_when_full_column_is_marked() {
         #[rustfmt::skip]
-        let state: Vec<(&u32, bool)> = vec![
-            (&21, false), (&23, true), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, true), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, true), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, true), (&11, false), (&54, false), (&95, false),
-            (&21, false), (&23, true), (&11, false), (&54, false), (&95, false),
+        let state: Vec<(u32, bool)> = vec![
+            (21, false), (23, true), (11, false), (54, false), (95, false),
+            (21, false), (23, true), (11, false), (54, false), (95, false),
+            (21, false), (23, true), (11, false), (54, false), (95, false),
+            (21, false), (23, true), (11, false), (54, false), (95, false),
+            (21, false), (23, true), (11, false), (54, false), (95, false),
         ];
 
         assert!(is_winner(&state));
